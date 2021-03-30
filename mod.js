2d1aff9301dd8029e8ee257abfa4674a435aa9e7
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-03-30 09:22:14 +0900

    Should be utf-8

--- mod.js
+++ mod.js
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/x/sift@0.1.6/mod.ts";
 
 serve({
-  "/": () => new Response("デノ"),
-  404: () => new Response("ありまへん"),
+  "/": () => new Response("デノ", {headers: {"content-type": "text/html; charset=utf-8"}}),
+  404: () => new Response("ありまへん", {headers: {"content-type": "text/html; charset=utf-8"}}),
 });
