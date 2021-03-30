5e37626966b954f38a6167ee7dbac9f093e2e09c
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-03-30 09:20:40 +0900

    Should be Response or callable

--- mod.js
+++ mod.js
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/x/sift@0.1.6/mod.ts";
 
 serve({
-  "/": "Hello!",
-  404: "Not Found",
+  "/": () => new Response("デノ"),
+  404: () => new Response("ありまへん"),
 });
