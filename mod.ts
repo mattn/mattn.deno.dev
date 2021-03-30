805b2b10d6f8c6a686f366e5e03b423e1bc953bd
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-03-30 13:12:07 +0900

    Fix

--- mod.ts
+++ mod.ts
@@ -1,5 +1,25 @@
 import { serve } from "https://deno.land/x/sift@0.1.6/mod.ts";
-import page from "./components/page.jsx";
+
+const page = (title: string, body: string): Response => new Response(`
+<title>${title}</title>
+<meta charset="utf-8"/>
+<style>
+body {
+  line-height: 1.5em;
+}
+main {
+  margin: 50vh auto 0;
+  transform: translateY(-50%);
+  padding: 15px 30px;
+  text-align: center;
+  font-size: 10em;
+  text-transform: uppercase;
+}
+</style>
+<main>
+${body}
+</main>
+`, {'headers': {'content-type': 'text/html; charset=utf-8'}});
 
 serve({
   "/": () => page("デノ", "デノ"),
