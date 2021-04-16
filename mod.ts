46770f869206565e545123e17fea4e6cd55752ed
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-04-16 16:38:11 +0900

    Update

--- mod.ts
+++ mod.ts
@@ -108,7 +108,7 @@ const faces = [
 serve({
   "/": () => page("デノ", "デノ"),
   "/install": () => {
-    return new Response(faces[Math.floor(Math.random() * ary.length)], {"headers": {"content-type": "text/css; charset=utf-8"}});
+    return new Response(faces[Math.floor(Math.random() * faces.length)], {"headers": {"content-type": "text/css; charset=utf-8"}});
   },
   404: () => page("デノ", "ありまへん"),
 });
